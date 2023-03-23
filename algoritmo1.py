d705b7576dd25c80846bdf8a9c121f4af421af8b
Jonkkeyler333 <keylersanchez00@gmail.com>
2023-03-23 06:26:52 -0500

    agregue la funcion

--- algoritmo1.py
+++ algoritmo1.py
@@ -1,1 +1,6 @@
-print("hello world")
+def calcular_porcentajes(mujeres:int,hombres:int)->int:
+    total=mujeres+hombres
+    total_mujeres=(mujeres*100)/total
+    total_hombres=(hombres*100)/total
+    return f'El numero de mujeres es {total_mujeres} y el número de hombres es {total_hombres}'
+
