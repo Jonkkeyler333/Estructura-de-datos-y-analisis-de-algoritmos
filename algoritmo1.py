42796cf2a94cdda86360fe3fe0c93dcd90277754
Jonkkeyler333 <keylersanchez00@gmail.com>
2023-03-23 06:35:38 -0500

    programa sin detalles , pero casi acabado

--- algoritmo1.py
+++ algoritmo1.py
@@ -1,6 +1,14 @@
-def calcular_porcentajes(mujeres:int,hombres:int)->int:
+def calcular_porcentajes(mujeres:int,hombres:int)->str:
     total=mujeres+hombres
     total_mujeres=(mujeres*100)/total
     total_hombres=(hombres*100)/total
     return f'El numero de mujeres es {total_mujeres} y el número de hombres es {total_hombres}'
 
+print("BIENVENIDO")
+try:
+    total_mujeres=int(input("por favor ingrese el número total de mujeres  :  "))
+    total_hombres=int(input("por favor ingrese el número total de hombres :  "))
+except TypeError:
+    print("Has ingresado un dato no valido")
+    
+print(calcular_porcentajes(total_mujeres,total_hombres))
